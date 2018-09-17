a8ef7125e7cb813999658b816c63d890aee2707d
panikutysh <pani.kutysh@gmail.com>
2018-09-17 15:07:57 +0300

    lovetriangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,41 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  NumberTriangles = 0;
+    for(i=1;i<=preferences.length;i++)
+    {
+      FirstElementIndex = i-1;
+      FirstElementValue = preferences[FirstElementIndex];
+      SecondElementIndex = FirstElementValue-1;
+      SecondElementValue = preferences[SecondElementIndex];
+      ThirdElementIndex = SecondElementValue-1;
+      ThirdElementValue = preferences[ThirdElementIndex];
+      if ((FirstElementValue == SecondElementIndex+1)&&(SecondElementValue == ThirdElementIndex+1)&&(ThirdElementValue == FirstElementIndex+1))
+      {
+        NumberTriangles = NumberTriangles + 1; 
+      } 
+    }  
+    if (NumberTriangles>=3)
+    {
+      NumberTriangles = Math.floor(NumberTriangles / 3);
+    }
+    else
+    {
+      NumberTriangles = 0;  
+    }
+  /* for(i=0;i<preferences.length;i++)
+  {
+  
+    FirstElementIndex = i;
+    FirstElementValue = preferences[FirstElementIndex];
+    SecondElementIndex = FirstElementValue-1;
+    SecondElementValue = preferences[SecondElementIndex];
+    ThirdElementIndex = SecondElementValue-1;
+    ThirdElementValue = preferences[ThirdElementIndex];
+    if (ThirdElementValue == FirstElementIndex)
+    {
+      NumberTriangles = NumberTriangles + 1; 
+    } 
+  }*/
+  return  NumberTriangles;
 };
